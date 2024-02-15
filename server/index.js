865a0978a11c9e132f5a4e8c66b2db3fcceac922
immadhavi <bhawsar.madhavi@gmail.com>
2024-02-16 04:21:03 +0530

    signin_complete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,42 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const app = express();
+// require("dotenv").config();
+// const { MONGO_URL, PORT } = process.env;
+// // const MONGO_URL = "mongodb+srv://note_summerizer_1018:note_summerizer_1018@cluster0.0cdsgi1.mongodb.net/?retryWrites=true&w=majority";
+// // const PORT = 4000;
+// mongoose
+//   .connect(MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB is  connected successfully"))
+//   .catch((err) => console.error(err));
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:4000"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-// const { MONGO_URL, PORT } = {"mongodb+srv://note_summerizer_1018:note_summerizer_1018@cluster0.0cdsgi1.mongodb.net/?retryWrites=true&w=majority",4000};
-const MONGO_URL="mongodb+srv://note_summerizer_1018:note_summerizer_1018@cluster0.0cdsgi1.mongodb.net/?retryWrites=true&w=majority";
-const PORT = 4000;
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+const { MONGO_URL, PORT } = process.env;
+
 mongoose
   .connect(MONGO_URL, {
     useNewUrlParser: true,
@@ -20,11 +51,13 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:4000"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+app.use(cookieParser());
 
 app.use(express.json());
 
+app.use("/", authRoute);
